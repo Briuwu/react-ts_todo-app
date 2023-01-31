2c855d174db4a0740626682829945ad8e7803be1
Briuwu <millontebrian@gmail.com>
2023-01-31 16:50:26 +0800

    new input will now appear at the top of the list.

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -6,9 +6,11 @@ import crossIcon from "../images/icon-cross.svg";
 const Tasks = () => {
   const { todos, completeTask, removeTask } = useContext(TodoContext);
 
+  const sortedTodos = todos.sort((a, b) => b.id - a.id);
+
   return (
     <div className="tasks">
-      {todos.map((item) => (
+      {sortedTodos.map((item) => (
         <div key={item.id} className="tasks__each">
           <button
             onClick={() => completeTask(item.id)}
